bbe2da0a859d65c2287a6fe1df15ecebbf72e210
hulksyed07 <hulk.syed07@gmail.com>
2023-01-14 17:57:57 +0530

    Moving All Search logic to separate Search Class (making controller thin)

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,32 +1,7 @@
 class BidsController < ApplicationController
   def index
-    searched_countries = params[:countries].split(',')
-    searched_categories = params[:categories].split(',')
-    searched_channels = params[:channels].split(',')
-
-    bids = []
-    searched_countries.each do |country|
-      searched_categories.each do |category|
-        searched_channels.each do |channel|
-          amount = Bid.select("amount")
-                      .where(country: [country, '*'], category: [category, '*'], channel: [channel, '*'])
-                      .order(Arel.sql("(CASE
-                                WHEN (country != '*' AND category != '*' AND channel != '*') THEN 1
-                                WHEN (country != '*' AND category != '*' AND channel = '*') THEN 2
-                                WHEN (country != '*' AND category = '*' AND channel != '*') THEN 3
-                                WHEN (country != '*' AND category = '*' AND channel = '*') THEN 4
-                                WHEN (country = '*' AND category != '*' AND channel != '*') THEN 5
-                                WHEN (country = '*' AND category != '*' AND channel = '*') THEN 6
-                                WHEN (country = '*' AND category = '*' AND channel != '*') THEN 7
-                                WHEN (country = '*' AND category = '*' AND channel = '*') THEN 8
-                                ELSE 8
-                              END)"))
-                      .first.amount
-          bids << { country: country, category: category, channel: channel, amount: amount }
-        end
-      end
-    end
-
-    render json: { bids: bids }
+    bid_search = BidSearch.new(params)
+    bid_search.call
+    render json: { bids: bid_search.bids }
   end
 end
---
+++ app/searches/bid_search.rb
@@ -0,0 +1,37 @@
+class BidSearch
+  attr_accessor :countries
+  attr_accessor :categories
+  attr_accessor :channels
+  attr_reader :bids
+
+  def initialize(params)
+    @countries = params[:countries].split(',')
+    @categories = params[:categories].split(',')
+    @channels = params[:channels].split(',')
+  end
+
+  def call
+    @bids = []
+    @countries.each do |country|
+      @categories.each do |category|
+        @channels.each do |channel|
+          amount = Bid.select("amount")
+                      .where(country: [country, '*'], category: [category, '*'], channel: [channel, '*'])
+                      .order(Arel.sql("(CASE
+                                WHEN (country != '*' AND category != '*' AND channel != '*') THEN 1
+                                WHEN (country != '*' AND category != '*' AND channel = '*') THEN 2
+                                WHEN (country != '*' AND category = '*' AND channel != '*') THEN 3
+                                WHEN (country != '*' AND category = '*' AND channel = '*') THEN 4
+                                WHEN (country = '*' AND category != '*' AND channel != '*') THEN 5
+                                WHEN (country = '*' AND category != '*' AND channel = '*') THEN 6
+                                WHEN (country = '*' AND category = '*' AND channel != '*') THEN 7
+                                WHEN (country = '*' AND category = '*' AND channel = '*') THEN 8
+                                ELSE 8
+                              END)"))
+                      .first.amount
+          @bids << { country: country, category: category, channel: channel, amount: amount }
+        end
+      end
+    end
+  end
+end
