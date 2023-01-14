e6b3d0e3d12f4156792a4976e2c8d19de0a67d87
hulksyed07 <hulk.syed07@gmail.com>
2023-01-14 17:45:41 +0530

    Using Active Record select and where instead of find_by_sql

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -8,25 +8,20 @@ class BidsController < ApplicationController
     searched_countries.each do |country|
       searched_categories.each do |category|
         searched_channels.each do |channel|
-          amount = Bid.find_by_sql(["SELECT amount
-                                    FROM
-                                    #{Bid.table_name}
-                                    where country in (?) AND category in (?) AND channel in (?)
-                                    ORDER BY (
-                                       CASE
-                                         WHEN (country != '*' AND category != '*' AND channel != '*') THEN 1
-                                         WHEN (country != '*' AND category != '*' AND channel = '*') THEN 2
-                                         WHEN (country != '*' AND category = '*' AND channel != '*') THEN 3
-                                         WHEN (country != '*' AND category = '*' AND channel = '*') THEN 4
-                                         WHEN (country = '*' AND category != '*' AND channel != '*') THEN 5
-                                         WHEN (country = '*' AND category != '*' AND channel = '*') THEN 6
-                                         WHEN (country = '*' AND category = '*' AND channel != '*') THEN 7
-                                         WHEN (country = '*' AND category = '*' AND channel = '*') THEN 8
-                                         ELSE 8
-                                       END
-                                    ) ASC
-                                    LIMIT 1
-                                 ", [country, '*'], [category, '*'], [channel, '*']]).first.amount
+          amount = Bid.select("amount")
+                      .where(country: [country, '*'], category: [category, '*'], channel: [channel, '*'])
+                      .order(Arel.sql("(CASE
+                                WHEN (country != '*' AND category != '*' AND channel != '*') THEN 1
+                                WHEN (country != '*' AND category != '*' AND channel = '*') THEN 2
+                                WHEN (country != '*' AND category = '*' AND channel != '*') THEN 3
+                                WHEN (country != '*' AND category = '*' AND channel = '*') THEN 4
+                                WHEN (country = '*' AND category != '*' AND channel != '*') THEN 5
+                                WHEN (country = '*' AND category != '*' AND channel = '*') THEN 6
+                                WHEN (country = '*' AND category = '*' AND channel != '*') THEN 7
+                                WHEN (country = '*' AND category = '*' AND channel = '*') THEN 8
+                                ELSE 8
+                              END)"))
+                      .first.amount
           bids << { country: country, category: category, channel: channel, amount: amount }
         end
       end
