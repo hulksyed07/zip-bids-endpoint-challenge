067b3dd54ff632e42282236c8bf4d2fe76393831
hulksyed07 <hulk.syed07@gmail.com>
2023-01-14 17:27:04 +0530

    Adding Acceptance spec in order to generate API Docs

---
+++ config/initializers/rspec_api_documentation.rb
@@ -0,0 +1,9 @@
+if Rails.env.test?
+  module RspecApiDocumentation
+    class RackTestClient < ClientBase
+      def response_body
+        last_response.body.encode("utf-8")
+      end
+    end
+  end
+end
