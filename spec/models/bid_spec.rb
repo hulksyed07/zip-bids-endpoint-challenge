af1de460628df68d0850eba2060bc1ae38e991f7
hulksyed07 <hulk.syed07@gmail.com>
2023-01-15 12:31:52 +0530

    Adding test case for Priority assignment

--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -15,4 +15,30 @@ RSpec.describe Bid, type: :model do
     Bid.last.destroy
     expect(Bid.count).to eq 3
   end
+
+  it 'should create bid and assign correct priority to it' do
+    bid = create(:bid)
+    expect(bid.priority).to eq 1
+
+    bid = create(:bid, channel: '*')
+    expect(bid.priority).to eq 2
+
+    bid = create(:bid, category: '*')
+    expect(bid.priority).to eq 3
+
+    bid = create(:bid, category: '*', channel: '*')
+    expect(bid.priority).to eq 4
+
+    bid = create(:bid, country: '*')
+    expect(bid.priority).to eq 5
+
+    bid = create(:bid, country: '*', channel: '*')
+    expect(bid.priority).to eq 6
+
+    bid = create(:bid, country: '*', category: '*')
+    expect(bid.priority).to eq 7
+
+    bid = create(:bid, country: '*', category: '*', channel: '*')
+    expect(bid.priority).to eq 8
+  end
 end
