860ebd668e4bc8de470cd6697f5306fb8e656667
hulksyed07 <hulk.syed07@gmail.com>
2023-01-15 12:31:22 +0530

    Using == instead of = (Previously this condition was written in SQL so = was working fine)

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -8,17 +8,17 @@ class Bid < ApplicationRecord
     self.priority = case
       when country != '*' && category != '*' && channel != '*'
         1
-      when country != '*' && category != '*' && channel = '*'
+      when country != '*' && category != '*' && channel == '*'
         2
-      when country != '*' && category = '*' && channel != '*'
+      when country != '*' && category == '*' && channel != '*'
         3
-      when country != '*' && category = '*' && channel = '*'
+      when country != '*' && category == '*' && channel == '*'
         4
-      when country = '*' && category != '*' && channel != '*'
+      when country == '*' && category != '*' && channel != '*'
         5
-      when country = '*' && category != '*' && channel = '*'
+      when country == '*' && category != '*' && channel == '*'
         6
-      when country = '*' && category = '*' && channel != '*'
+      when country == '*' && category == '*' && channel != '*'
         7
       else
         8
